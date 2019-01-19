23fa2c0695444dd5f83cad5e17068096bf77a30f
Jason Graalum <jasongraalum@gmail.com>
2019-01-19 14:37:49 -0800

    Added simple sum function - not working

--- HW2/pthread/summation/main.c
+++ HW2/pthread/summation/main.c
@@ -12,7 +12,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void main ( int argc, char **argv ) {
+void *sum_thread(void *vargp);
+
+typedef unsigned long data_t;
+volatile data_t global_sum;
+size_t nelems_per_thread;
+size_t nthreads;
+
+int main ( int argc, char **argv ) {
+
+  global_sum = 0;
 
   if(argc < 2) { 
     printf("Usage: %s n k\n",argv[0]); 
@@ -21,14 +30,39 @@ void main ( int argc, char **argv ) {
 
   pthread_t *tid;
 
-  typedef unsigned long data_t;
-  volatile data_t global_sum;
-
   size_t number = (size_t) atoi(argv[1]);
   size_t nthreads = (size_t) atoi(argv[2]);
 
-  size_t nelems_per_thread = number/nthreads;
+  nelems_per_thread = number/nthreads;
+
+  tid = (pthread_t *) malloc(sizeof(pthread_t)*nthreads);
+
+  int *myid;
+  myid = (int *)malloc(sizeof(int)*nthreads);
 
   printf("number = %ld\tthreads = %ld\telems_per_thread = %ld\n", number, nthreads, nelems_per_thread);
+
+/* Create threads and wait for them to finish */
+  for (int i = 0; i < nthreads; i++) {
+    myid[i] = i;
+    Pthread_create(&tid[i], NULL, sum_thread, &myid[i]);
+  }
+  for (int i = 0; i < nthreads; i++)
+    Pthread_join(tid[i], NULL);
+
 }
+
+void *sum_thread(void *vargp)
+{
+    int myid = *((int *)vargp);
+    size_t start = myid * nelems_per_thread;
+    size_t end = start + nelems_per_thread;
+    size_t i;
+    for (i = start; i < end; i++) {
+       global_sum += i;
+}
+    return NULL;
+}
+
+
 
